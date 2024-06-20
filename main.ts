517ed908f884fafbe6a27c1d69ff01224c30fda9
pema124 <peter.mathes@gmx.de>
2024-06-20 22:02:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function SerielleAusgabe () {
+    serial.writeValue("l1= ", l1)
+    serial.writeValue("l2= ", l2)
+    serial.writeValue("l3= ", l3)
+    serial.writeValue("l4= ", l4)
+    serial.writeValue("l5= ", l5)
+    serial.writeValue("l6= ", l6)
+    serial.writeValue("lenkwinkel= ", lenkwinkel)
+}
+let beschleunigung_y = 0
+let beschleunigung_x = 0
 let lenkwinkel = 0
 let l6 = 0
 let l5 = 0
@@ -5,8 +16,6 @@ let l4 = 0
 let l3 = 0
 let l2 = 0
 let l1 = 0
-let beschleunigung_y = 0
-let beschleunigung_x = 0
 serial.redirectToUSB()
 let motor_max = 255
 let motor_l = 0
@@ -23,16 +32,10 @@ loops.everyInterval(100, function () {
     l5 = beschleunigung_x / l4
     l6 = -1 * l5
     lenkwinkel = Math.acos(l6)
-    serial.writeValue("l1= ", l1)
-    serial.writeValue("l2= ", l2)
-    serial.writeValue("l3= ", l3)
-    serial.writeValue("l4= ", l4)
-    serial.writeValue("l5= ", l5)
-    serial.writeValue("l6= ", l6)
-    serial.writeValue("lenkwinkel= ", lenkwinkel)
+    SerielleAusgabe()
     geschw_red = Math.abs(beschleunigung_x) / 1023 * motor_max
     if (Math.abs(beschleunigung_x) > nullpunkt_tol) {
-        if (beschleunigung_x < 0) {
+        if (0 < 0) {
             motor_l = motor_l - geschw_red
         }
         if (beschleunigung_x > 0) {
