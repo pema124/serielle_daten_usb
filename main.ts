ed7b445070f815346bf6c54e11214048703d6efa
pema124 <peter.mathes@gmx.de>
2024-06-20 20:55:29 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,29 @@
+let lenkwinkel = 0
+let l6 = 0
+let l5 = 0
+let l4 = 0
+let l3 = 0
+let l2 = 0
+let l1 = 0
+let beschleunigung_y = 0
+let beschleunigung_x = 0
+serial.redirectToUSB()
 let motor_max = 255
 let motor_l = 0
 let motor_r = 0
 let nullpunkt_tol = 10
-let beschleunigung_x = 0
 let geschw_red = 0
 loops.everyInterval(100, function () {
-    if (true) {
-        motor_l = motor_max
-        motor_r = motor_max
-    }
     beschleunigung_x = input.acceleration(Dimension.X)
+    beschleunigung_y = input.acceleration(Dimension.Y)
+    l1 = beschleunigung_x ** 2
+    l2 = beschleunigung_y ** 2
+    l3 = l1 + l2
+    l4 = Math.sqrt(l3)
+    l5 = beschleunigung_x / l4
+    l6 = -1 * l5
+    lenkwinkel = Math.acos(l6)
+    serial.writeValue("lenkwinkel= ", lenkwinkel)
     geschw_red = Math.abs(beschleunigung_x) / 1023 * motor_max
     if (Math.abs(beschleunigung_x) > nullpunkt_tol) {
         if (beschleunigung_x < 0) {
